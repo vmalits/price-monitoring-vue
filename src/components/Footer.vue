0b88aeb6bdefbc4e766ac15d4a0fda447c591d80
Vladimir Malits <vladimir.malits@gmail.com>
2020-10-19 16:34:40 +0300

    added Header.vue , NavBar.vue, Footer.vue, components, removed HelloWorld.vue, refactored the base structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer color="deep-purple" app>
+    <span class="white--text"
+      >Copyright &copy; Price Monitoring {{ currentYear }}</span
+    >
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app color="deep-purple" dark>
+    <v-app-bar-nav-icon
+      @click.stop="$emit('toggle-drawer')"
+    ></v-app-bar-nav-icon>
+    <v-toolbar-title>Shop</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped></style>
